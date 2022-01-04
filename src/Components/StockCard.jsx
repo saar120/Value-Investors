ac3bd800353b6ec962dc4207b2df42e6e881167d
saar120 <saar120@gmail.com>
2022-01-04 10:46:08 +0200

    added stock card

--- src/Components/StockCard.jsx
+++ src/Components/StockCard.jsx
@@ -1,19 +1,58 @@
 import React from "react";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import styled from "styled-components";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const StyledStockCard = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 0.6rem;
+  gap: 0.5rem;
+  .title {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+  .stock-name {
+    font-weight: bold;
+  }
+`;
 
 export default function StockCard({ stock }) {
+  const rows = [
+    { propery: "Stock Price:", value: "$" + stock.currentPrice },
+    { propery: "Market Cap:", value: stock.currentPrice },
+    { propery: "Book Value:", value: stock.bookValue },
+    { propery: "Free Cash Flow:", value: stock.freeCashFlow },
+    { propery: "Industry:", value: stock.industry },
+    { propery: "Analysts Recommended:", value: stock.recommendation.toUpperCase() },
+    { propery: "Mid Term Trend:", value: stock.midTermTrend },
+  ];
+
   return (
-    <div>
-      <div>
-        <AttachMoneyIcon />
-        {stock.name}
-      </div>
-      <div>{stock.price}</div>
-      <div>Market cap: {stock.mktCap}</div>
-      <div>industry: {stock.industry}</div>
-      <div>recommendation: {stock.recommendation}</div>
-      <div>bookValue: {stock.bookValue}</div>
-      <div>trend: {stock.midTermTrend}</div>
-    </div>
+    <StyledStockCard>
+      <div className="title">Stock Data</div>
+      <div className="stock-name">{stock.name}</div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 250 }} aria-label="simple table">
+          <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow key={row.propery}>
+                  <TableCell component="th" scope="row" sx={{ fontWeight: "bold" }}>
+                    {row.propery}
+                  </TableCell>
+                  <TableCell align="left">{row.value}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </StyledStockCard>
   );
 }
